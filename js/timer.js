5f2daf7f6bf9f40715d1086427be004c25b48c87
sunny <mikus1337112@gmail.com>
2022-09-03 17:31:01 +0300

    timer, mobile landscape optimization

---
+++ js/timer.js
@@ -0,0 +1,41 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 4, 2022 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes
+  }
+
+ // Display the result in the element with id="timer"
+
+  if (days === 0) {
+    document.getElementById("timer").innerHTML = hours + " : "
+    + minutes + " : " + seconds + "";
+  } else {
+    document.getElementById("timer").innerHTML = days + "<br>" + hours + " : "
+    + minutes + " : " + seconds + "";
+  }
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "&#127874; yay party &#127874;";
+  }
+}, 1000);
